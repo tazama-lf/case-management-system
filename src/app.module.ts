0bc1939a927ed8d40805d752cc4caa64e2ab34a0
mataa019 <johnnymataa019@gmail.com>
2025-07-29 08:42:59 +0200

    feat/prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import { AuditLogModule } from './audit/auditLog.module';
 import { TriageModule } from './triage/triage.module';
 import { PrismaModule } from './prisma.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaController } from './prisma/prisma.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [
@@ -14,5 +17,7 @@ import { PrismaModule } from './prisma.module';
     AuditLogModule,
     TriageModule,
   ],
+  providers: [PrismaService],
+  controllers: [PrismaController],
 })
 export class AppModule {}
