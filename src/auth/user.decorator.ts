f28347495bfd9c2237de00fb36a15488fc93ba39
mataa019 <johnnymataa019@gmail.com>
2025-07-29 16:14:39 +0200

    feat:auth service

--- src/auth/user.decorator.ts
+++ src/auth/user.decorator.ts
@@ -1,6 +1,15 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+<<<<<<< HEAD
 export const User = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
   return request.user;
 });
+=======
+export const User = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+  },
+);
+>>>>>>> aea5c90 (feat:auth)
