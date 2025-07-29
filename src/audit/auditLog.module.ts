fd5a237fe5d336eed96d5229262789c39a788ba5
mataa019 <johnnymataa019@gmail.com>
2025-07-29 16:09:19 +0200

    feat:auth

--- src/audit/auditLog.module.ts
+++ src/audit/auditLog.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuditLogService } from './auditLog.service';
-import { PrismaModule } from '../prisma.module';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule,],
+  imports: [PrismaModule],
   providers: [AuditLogService],
   controllers: [],
   exports: [AuditLogService],
