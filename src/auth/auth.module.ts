d139c1cd60e2904f1413aa9f4d5f24b657f1a43f
mataa019 <johnnymataa019@gmail.com>
2025-07-29 16:17:13 +0200

    feat:auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [PassportModule],
   providers: [JwtStrategy, AuthService],
   exports: [PassportModule, JwtStrategy, AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
