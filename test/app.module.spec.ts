aa0ee98837e9ee61ea41013fc560e6e2ccf163bb
mataa019 <johnnymataa019@gmail.com>
2025-08-05 14:47:05 +0200

    feat: token refresh functionality implemented

--- test/app.module.spec.ts
+++ test/app.module.spec.ts
@@ -1,8 +1,16 @@
+import { Logger } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { PrismaService } from '../prisma/prisma.service';
 
 describe('AppModule', () => {
+  beforeAll(() => {
+    jest.spyOn(Logger.prototype, 'log').mockImplementation(jest.fn());
+    jest.spyOn(Logger.prototype, 'error').mockImplementation(jest.fn());
+    jest.spyOn(Logger.prototype, 'warn').mockImplementation(jest.fn());
+    jest.spyOn(Logger.prototype, 'debug').mockImplementation(jest.fn());
+    jest.spyOn(Logger.prototype, 'verbose').mockImplementation(jest.fn());
+  });
   let module: TestingModule;
 
   beforeEach(async () => {
