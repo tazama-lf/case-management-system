b1dc73edf9fe1dbb5f95a69a8007d4ed6d9fc03e
mataa019 <johnnymataa019@gmail.com>
2025-08-12 14:39:08 +0200

    fix:jest.config.js to jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,3 @@
-// SPDX-License-Identifier: Apache-2.0
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
 import type { Config } from '@jest/types';
 // Sync object
 const config: Config.InitialOptions = {
