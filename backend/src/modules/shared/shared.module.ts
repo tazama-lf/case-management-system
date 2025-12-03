80445bf944a037b497d4e5e58539ba3c6319893e
ReebaPaysys <reeba.siddiqui@paysyslabs.com>
2025-12-03 10:41:45 +0500

    fix: add logs

--- backend/src/modules/shared/shared.module.ts
+++ backend/src/modules/shared/shared.module.ts
@@ -1,14 +1,16 @@
-import { Module, Global } from '@nestjs/common';
+import { Module, Global, forwardRef } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 import { CasePriorityUtil } from './utils/case-priority.util';
 import { RedisService } from './redis.service';
 import { CacheService } from './cache.service';
 import { AuthModule } from '../auth/auth.module';
+import { UserService } from './user.service';
 
 @Global()
 @Module({
-  imports: [ConfigModule, AuthModule],
-  providers: [CasePriorityUtil, RedisService, CacheService],
-  exports: [CasePriorityUtil, RedisService, CacheService],
+  imports: [ConfigModule, AuthModule, HttpModule],
+  providers: [CasePriorityUtil, RedisService, CacheService, UserService],
+  exports: [CasePriorityUtil, RedisService, CacheService, UserService],
 })
 export class SharedModule {}
