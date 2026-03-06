ec57c1c2b13af7b9905b57d245a072484544a153
Syed Hassan Rizwan <hassan.rizwan@paysyslabs.com>
2026-03-06 16:15:55 +0500

    fix: remove unused init.sql file and update server configuration in vite.config.ts

--- init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE flowable;
-CREATE DATABASE tazama_dwh;
-CREATE DATABASE tazama_cms;
-
--- npx prisma migrate dev --schema=prismaDWH/schema.dwh.prisma
--- npx prisma migrate dev --name init
-
-
--- npx prisma generate --schema=prismaDWH/schema.dwh.prisma
