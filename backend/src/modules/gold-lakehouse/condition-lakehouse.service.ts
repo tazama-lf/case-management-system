9fbaf05d98f569e62e97315b8341cd632f1421da
Sobia Rizwan <sobia.rizwan@paysyslabs.com>
2026-09-10 12:34:30 +0500

    fix: condition query issue
    Signed-off-by: Sobia Rizwan <sobia.rizwan@paysyslabs.com>

--- backend/src/modules/gold-lakehouse/condition-lakehouse.service.ts
+++ backend/src/modules/gold-lakehouse/condition-lakehouse.service.ts
@@ -100,12 +100,10 @@ export class ConditionLakehouseService extends GoldLakehouseService {
   ): Promise<ConditionsContextByTransactionResponse> {
     try {
       const txSql =
-        'SELECT transaction_id, end_to_end_id, tx_event_ts, tx_event_date, tx_type, interbank_settlement_amount, interbank_settlement_currency, debtor_id, debtor_name, debtor_account_id, creditor_id, creditor_name, creditor_account_id FROM transaction_detail WHERE end_to_end_id = $1 AND tenant_id = $2 LIMIT 100;';
+        'SELECT transaction_id, end_to_end_id, tx_event_ts, tx_event_date, tx_type, interbank_settlement_amount, interbank_settlement_currency, debtor_id, debtor_name, debtor_account_id, creditor_id, creditor_name, creditor_account_id FROM transaction_detail WHERE end_to_end_id = $1 AND tenant_id = $2;';
 
-      const txResponse = await this.runSqlQuery(txSql, 1, [transactionId, tenantId], userJwt);
+      const txResponse = await this.runSqlQuery(txSql, 100, [transactionId, tenantId], userJwt);
       const pacs8 = txResponse.data.find((record) => record.tx_type === 'pacs.008.001.10');
-
-      this.logger.log(`Transaction details for ${transactionId}:`, txResponse.data);
 
       if (!pacs8) {
         throw new HttpException(`Transaction ${transactionId} not found`, HttpStatus.NOT_FOUND);
